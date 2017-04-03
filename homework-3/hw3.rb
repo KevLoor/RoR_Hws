0ee1e7d174e8ffcb96f3002c66259b5b45263457
Kevin Loor <lkloor@gmail.com>
2017-04-03 18:33:24 -0400

    homework3

---
+++ homework-3/hw3.rb
@@ -0,0 +1,50 @@
+#PROBLEM1
+# def count_words(phrase)
+#   frequency = {}
+#   array_of_words = phrase.scan(/\w+/)
+#   array_of_words.each do |word, times|
+#     frequency[word]  += 1
+#   end
+# end
+#
+# puts(count_words('it was the best of times, it was the worst of times'))
+
+#  PROBLEM2
+# class Animal
+#   def sound
+#     'meow'
+#   end
+#
+#   def speak
+#     puts('The animal says: ' + sound)
+#   end
+# end
+#
+# class Canine < Animal
+#   attr_accessor(:breed)
+#   attr_accessor(:bark)
+#   def description
+#     "I am a #{breed}, #{bark}!!!"
+#   end
+# end
+#
+# class Feline < Animal
+#   attr_accessor(:cat_type)
+#   attr_accessor(:cat_noise)
+#   def description
+#     "I am a #{cat_type}, #{cat_noise}"
+#   end
+# end
+#
+# dalmatian = Canine.new
+# dalmatian.breed = 'dalmatian'
+# dalmatian.bark = 'WOOF!!'
+# puts(dalmatian.description)
+#
+# kitty = Feline.new
+# kitty.cat_type = 'kitty cat'
+# kitty.cat_noise = 'MEOW!'
+# puts(kitty.description)
+
+#PROBLEM$
+# Difference between a GET and a POST request is that a GET request is used by the user to optain information from a server.  A POST request submits data to the server.
